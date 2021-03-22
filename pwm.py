567507deb1840478263ed5e84032ad1c4be0b0e7
EmbeddedIcon <72641093+EmbeddedIcon@users.noreply.github.com>
2021-03-23 02:11:54 +0530

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -1,21 +1,26 @@
-from machine import Pin, PWM
 import time
+from machine import Pin, PWM
 
-pwm = PWM(Pin(25))# Construct PWM object, with LED on Pin(25).
+pwm = PWM(Pin(15))  # GPO15  connect Logic Analyzer
+pwm1 = PWM(Pin(25)) #LED Onboard LED
 
-pwm.freq(1000)# Set the PWM frequency.
+pwm.freq(100)  # 10ms
+pwm1.freq(1000)  # 1ms for LED
 
 duty = 0
 direction = 1
 
-#for i in range( 16 *256 ):
-while True:
+while True: 
+    #for i in range(256):
         duty += direction
-        if duty > 255:
-            duty = 255
+        if duty > 10:
+            duty = 10
             direction = -1
-        elif duty < 0: 
+        elif duty < 0:
             duty = 0
             direction = 1
-        pwm.duty_u16(duty * duty)
-        time.sleep(0.001)
+        pwm.duty_u16(duty * 6553)
+        pwm1.duty_u16(duty * 6553)      # LED will show this output
+        time.sleep(0.001)         # 1ms delay
+
+
